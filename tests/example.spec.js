c04395cca4f12290e9a2afd1309647a1dae0b009
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-12 20:25:28 +0100

    Iterate through array of products, add one to cart

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,10 +3,20 @@ import userData from "../protected_data/protected_data.js";
 const { test, expect } = require('@playwright/test');
 
 test('Client app login', async ({ page }) => {
+    const products = page.locator(".card-body");
     await page.goto("https://rahulshettyacademy.com/client");
     await page.locator("#userEmail").fill(userData.userEmail);
     await page.locator("#userPassword").fill(userData.userPassword);
     await page.locator("[value='Login']").click();
     await page.waitForLoadState('networkidle');
+    const titles = await page.locator(".card-body b").allTextContents();
+    // console.log(titles);
+    const count = await products.count();
+    for(let i= 0; i < count; i++) {
+        if(await products.nth(i).locator("b").textContent() === "ZARA COAT 3") {
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+    }
     await page.pause();
 });
