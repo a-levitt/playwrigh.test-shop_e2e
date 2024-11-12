e4a13be86b8f819f573fefbb60e4ab4a70b90786
Artem Trifonov <artemis.levitt@gmail.com>
2024-11-12 19:25:00 +0100

    Login to app

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,12 @@
+import userData from "../protected_data/protected_data.js";
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Client app login', async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill(userData.userEmail);
+    await page.locator("#userPassword").fill(userData.userPassword);
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    await page.pause();
 });
